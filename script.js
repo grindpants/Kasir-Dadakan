3b58feb395a55e2dd57fd10b6698fe0d4a51ee7a
grindpants <mustaqinishak2@gmail.com>
2019-09-23 08:16:55 +0700

    tambah bosstrap dan file js

---
+++ script.js
@@ -0,0 +1,96 @@
+var pilih = document.getElementById("barang");
+var tambah = document.getElementById("add");
+var hapus = document.getElementById("delete");
+var total = document.getElementById("jumlahKeseluruhan");
+
+document.getElementById("jumlah").value = "";
+
+
+
+
+pilih.addEventListener('click', function(){
+    
+    var nilai = document.getElementById("barang").value;
+    var jumlah = document.getElementById("jumlah").value;
+    var x = document.getElementById("x");
+    x.value = nilai;
+    
+    
+});
+
+tambah.addEventListener('click', function(){
+    var daftarPesanan = document.getElementById("daftarPesanan"); //parent
+    var namaBarang = document.querySelectorAll("#barang option");
+    var nilai = document.getElementById("barang").value;
+    var jumlah = document.getElementById("jumlah").value;
+    var harga = document.getElementById("x").value;
+    var totalKeseluruhan = document.getElementById("totalSementara");
+    
+
+    
+
+    var NewRow = daftarPesanan.insertRow(-1); 
+    var Newcell1 = NewRow.insertCell(0); 
+    var Newcell2 = NewRow.insertCell(1);
+    var Newcell3 = NewRow.insertCell(2);
+    var Newcell4 = NewRow.insertCell(3);
+    
+
+    
+    for(let i = 0; i < namaBarang.length; i++){
+        if(nilai === namaBarang[i].value){
+            // insert new row. 
+            Newcell1.innerHTML = namaBarang[i].innerHTML;
+            Newcell2.innerHTML = nilai;
+            Newcell3.innerHTML = jumlah;
+            Newcell4.innerHTML = harga*jumlah;
+            Newcell4.value = harga*jumlah;
+        }
+        
+    } 
+    calculateTotal()
+    
+});
+
+hapus.addEventListener('click', function() { 
+    var totalKeseluruhan = document.getElementById("totalSementara");
+    // delete row (index-0).             
+    /* var nilai = totalKeseluruhan.value;
+
+            nilai -= Number(daftarPesanan.children[i].children[3].innerText)
+            totalKeseluruhan.value = nilai; */
+            
+    
+    document.getElementById("daftarPesanan").deleteRow(-1); 
+    calculateTotal()
+
+});
+
+total.addEventListener('click', function() {
+    var total = document.getElementById("totalSementara");
+    var nilaiDiskon = document.getElementById("diskon");
+    var totalKeseluruhan = document.getElementById("sisa");
+    var nilaiDiskon ;
+    if (nilaiDiskon.value == 0) {
+        totalKeseluruhan.value = Number(total.value)
+    } else {
+        nilaiDiskon = (Number(total.value) * nilaiDiskon.value) / 100;
+        totalKeseluruhan.value = Number(total.value) - nilaiDiskon;
+    }         
+});
+
+var calculateTotal = () => {
+    var daftarPesanan = document.getElementById("daftarPesanan");
+    var totalSementara = document.getElementById("totalSementara");
+    // var totalKeseluruhan = document.getElementById("sisa");
+    var nilai = 0;
+    for (var i = 0; i < daftarPesanan.children.length; i++){
+        nilai += Number(daftarPesanan.children[i].children[3].innerText)
+    }
+    totalSementara.value = nilai;
+    // totalKeseluruhan.value = nilai;
+
+
+}
+
+
